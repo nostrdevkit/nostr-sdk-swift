0c60d43fe0a92f6e8e3dbde29093a799d5ee6c8c
Anka <runner@sjc20-bb711-a033ec26-75b3-4537-9aba-a5327231df8c-16829343A42E.local>
2026-01-29 10:56:59 +0000

    Bump to 0.44.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "nostr_sdkFFI", url: "https://github.com/rust-nostr/nostr-sdk-swift/releases/download/0.44.0/nostr_sdkFFI.xcframework.zip", checksum: "fe25809c26f67fe3673da5a75f7ed985ce9056ea7540a06f340cf7da01c6aaea"),
+        .binaryTarget(name: "nostr_sdkFFI", url: "https://github.com/rust-nostr/nostr-sdk-swift/releases/download/0.44.2/nostr_sdkFFI.xcframework.zip", checksum: "3a3d527eea38a1f78b82ea4e3637445d07ce9fc861e99f660f6bb00a75d48f05"),
         .target(name: "NostrSDK", dependencies: ["nostr_sdkFFI"]),
     ]
 )
