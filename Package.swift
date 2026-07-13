202a1b1a7a6a0292767eb612f63e1f95fbd61ec0
Yuki Kishimoto <yukikishimoto@protonmail.com>
2026-07-13 10:03:16 +0200

    Update binary target URL for nostr_sdkFFI

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "nostr_sdkFFI", url: "https://github.com/rust-nostr/nostr-sdk-swift/releases/download/0.44.2/nostr_sdkFFI.xcframework.zip", checksum: "3a3d527eea38a1f78b82ea4e3637445d07ce9fc861e99f660f6bb00a75d48f05"),
+        .binaryTarget(name: "nostr_sdkFFI", url: "https://github.com/nostrdevkit/nostr-sdk-swift/releases/download/0.44.2/nostr_sdkFFI.xcframework.zip", checksum: "3a3d527eea38a1f78b82ea4e3637445d07ce9fc861e99f660f6bb00a75d48f05"),
         .target(name: "NostrSDK", dependencies: ["nostr_sdkFFI"]),
     ]
 )
